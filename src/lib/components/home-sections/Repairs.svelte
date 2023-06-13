4071948e68385ed449762251e060a4bd8ee46efa
DylanOrr-Uni <contact@dylanorr.com>
2023-06-13 01:14:43 -0400

    Updates

--- src/lib/components/home-sections/Repairs.svelte
+++
@@ -1,7 +0,0 @@
-<section class="repairs home-section mx-0 text-light" style="height: 40vh; background-image: url(../PhoneParts.png), linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.8); background-blend-mode: overlay; background-position: center; background-size: cover;" id="scrollspyHeadingGuides">
-     <div class="h-100 w-100 p-5 d-flex flex-column align-items-center text-center justify-content-center" style="background-color: rgba(255,193,7,0.4);">
-          <h2 class="mb-3">Repair Guides</h2>
-          <p class="mb-3">We have thousands of comprehensive step-by-step guides for everything from the latest iPhone to a circuit breaker frmo 1997.</p>
-          <a href="./guides" class="btn btn-primary">Repair Guides</a>
-     </div>
-</section>
